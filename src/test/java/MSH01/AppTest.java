b7bae9549c76207081b6fbf2e26e850a6f163787
RKUN <rkhan.qa@gmail.com>
2020-06-02 15:11:28 +0100

    4th commit - Local Machine RK

--- src/test/java/MSH01/AppTest.java
+++ src/test/java/MSH01/AppTest.java
@@ -19,6 +19,7 @@ public class AppTest
         System.out.println("Test has been executed 2");
         System.out.println("Test has been executed 3");
         System.out.println("Test has been executed 4");
+        System.out.println("Test has been executed 5");
 
 
     }
