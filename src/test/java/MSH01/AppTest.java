3468d9b773f5dfa5c4064af3cd3f0d273eb812a8
rk-qa <66306435+rk-qa@users.noreply.github.com>
2020-06-05 16:24:24 +0100

    Update AppTest.java
    9th commit from repo

--- src/test/java/MSH01/AppTest.java
+++ src/test/java/MSH01/AppTest.java
@@ -23,6 +23,8 @@ public class AppTest
         System.out.println("Test has been executed 6");
         System.out.println("Test has been executed 7");
         System.out.println("Test has been executed 8");
+                System.out.println("Test has been executed 9");
+
 
 
     }
